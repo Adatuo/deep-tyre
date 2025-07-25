5626693b5409f9277b298bebae33b250521118ef
wulinyi <wulinyi22@gmail.com>
2025-07-25 22:13:37 +0800

    feat: 前后端联调成功

---
+++ deeptyre/src/app/api/create-chat/route.ts
@@ -0,0 +1,12 @@
+import { createChat } from "@/index";
+import { auth } from "@clerk/nextjs/server";
+
+export async function POST(req: Request) {
+  const { title, model } = await req.json()
+  const { userId } = await auth()
+
+  if (userId) {
+    const newChat = await createChat(title, model, userId)
+    return new Response(JSON.stringify({id:newChat?.id}), { status: 200 })
+  }
+}
